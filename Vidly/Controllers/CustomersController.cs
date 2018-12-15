36773e4c4934b88ccd04641f1c0765e34e2caf2a
Kara Bharat <karabharat@gmail.com>
2018-12-15 11:20:30 +0530

    added migrations

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customerViewModel = new CustomerViewModels();
+            var customers = new List<Customer>();
+
+            customers.Add(new Customer { Id = 1, Name = "Bharat Kara" });
+            customers.Add(new Customer { Id = 2, Name = "John Smith" });
+
+            customerViewModel.Customers = customers;
+
+            return View(customerViewModel);
+        }
+
+        [Route("customers/details/{Id}")]
+        public ActionResult Detail(int Id)
+        {
+
+            var customerViewModel = new CustomerViewModels();
+            var customers = new List<Customer>();
+
+            customers.Add(new Customer { Id = 1, Name = "Bharat Kara" });
+            customers.Add(new Customer { Id = 2, Name = "John Smith" });
+
+
+            var customer = customers.Where(s => s.Id == Id).FirstOrDefault();
+
+            if (customer != null)
+            {
+                return View(customer);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,16 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+        public ActionResult Index()
         {
-            var movie = new Movie() {Name = "Inceptions"};
-            return View(movie);
+            var movieViewModel = new MovieViewModels();
+            var movies = new List<Movie>();
+
+            movies.Add(new Movie { Id = 1, Name = "Dead Pool" });
+            movies.Add(new Movie { Id = 2, Name = "Die Hard" });
+
+            movieViewModel.Movies = movies;
+
+            return View(movieViewModel);
         }
     }
 }
