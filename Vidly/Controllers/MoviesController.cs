cd325be47ea541ae18ab2295df36daf567839568
Kara Bharat <karabharat@gmail.com>
2018-12-19 08:53:35 +0530

    Movie save & edit
    Movie form

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -21,7 +21,7 @@ namespace Vidly.Controllers
         public ActionResult Index()
         {
             var movieViewModel = new MovieViewModels();
-            
+
             movieViewModel.Movies = _dbContext.Movie.Include(m => m.Genre).ToList();
 
             return View(movieViewModel);
@@ -38,6 +38,19 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
+        [Route("movies/edit/{Id}")]
+        public ActionResult Edit(int Id)
+        {
+            var movie = _dbContext.Movie.Where(s => s.Id == Id).Include(c => c.Genre).FirstOrDefault();
+
+            var editMovie = new MovieFormViewModels()
+            {
+                Movie = movie,
+                Genre = _dbContext.Genre.ToList()
+            };
+            return View("MovieForm", editMovie);
+        }
+
         public ActionResult New()
         {
             var newMovie = new MovieFormViewModels()
@@ -45,6 +58,27 @@ namespace Vidly.Controllers
                 Genre = _dbContext.Genre.ToList()
             };
             return View("MovieForm", newMovie);
+        }
+
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.AddedOn = DateTime.Now;
+                _dbContext.Movie.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _dbContext.Movie.Where(s => s.Id == movie.Id).Include(c => c.Genre).First();
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
